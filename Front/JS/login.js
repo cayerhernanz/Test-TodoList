e6fc20a7a97157bed23427c40e0f01b43a45a1e3
Cayetano <cayetano.rhernanz@gmail.com>
2023-08-03 14:40:28 +0200

    fonction login manque test

--- Front/JS/login.js
+++ Front/JS/login.js
@@ -1,8 +1,7 @@
 //Login dans le site
 //Variables
-let userNickname = document.getElementById("nickname").value;
-let userPassword = document.getElementById("password").value;
 let loginBtn = document.getElementById("loginBtn");
+let errorMsg = document.getElementById("errorMsg");
 
 //Verification que l'utilsateur n'est pas déjà connecté (jwt)
 function logVerification(){ 
@@ -14,5 +13,30 @@ function logVerification(){
 logVerification();
 
 function login(){
-
+    //Requête à l'API (en attendant qu'elle soit fonctionnelle)
+    let userNickname = document.getElementById("nickname").value;
+    let userPassword = document.getElementById("password").value;
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('POST', "API");
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(
+        JSON.stringify({
+            nickname: userNickname,
+            password: userPassword,
+        })
+    );
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4){
+            const objects = JSON.parse(this.responseText);
+            console.log(objects);
+            if(objects["status"] == "ok"){
+                localStorage.setItem("jwt", objects["accessToken"]);
+                window.location.href = "homepage.html";
+            }
+            else{
+                errorMsg.innerHTML = "Nickname et/ou mot de passe incorrect(s)!"
+            }
+        }
+    };
+    return false;
 }
